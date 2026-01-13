71dde8663c0f9305968537219005622b8441cb61
Shezhi <1052961520@qq.com>
2026-01-13 15:37:11 +0800

    fix：修复数据源新增调用禁用数据源接口的bug

--- views/DataSource/components/SourceDetailsAdd.vue
+++ views/DataSource/components/SourceDetailsAdd.vue
@@ -362,7 +362,7 @@ const handleSubmit = async () => {
   formRef.value
     .validate()
     .then(async () => {
-      if (formData.value.id) {
+      if (isEditor.value) {
         await disableDataSource(formData.value.id)
       }
       formItemRef
@@ -377,7 +377,7 @@ const handleSubmit = async () => {
           })
         })
         .finally(async () => {
-          if (formData.value.id) {
+          if (isEditor.value) {
             await enableDataSource(formData.value.id)
           }
         })
